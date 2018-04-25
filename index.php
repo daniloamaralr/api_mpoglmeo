23e67132da48f5cd35c682cfc8c8df7264e6d88a
unknown <daniloamaralr@gmail.com>
2018-04-25 11:02:39 -0300

    montando json

--- index.php
+++ index.php
@@ -1,7 +1,34 @@
 <?php
+$dsn = "mysql:dbname=mpog_lmeo;host=127.0.0.1";
+$dbuser = "root";
+$dbpass = "";
 
-$data = array("data" => "1975-02-01", "lat" => "-20.1847", "long" => "-44.8933");                                                                    
-$data_string = json_encode($data);
+try{
+    $pdo = new PDO($dsn,$dbuser,$dbpass);
+    //echo("Conexão estabelecida com sucesso");
+} catch(PDO_EXCEPTION $e){
+    echo "Falhou: ".$e->getMessage();
+}
+
+try{
+    $sql = "SELECT Latitude, Longitude, estacao.Nome as EstacaoNome, bacia.Nome as BaciaNome from estacao
+            INNER JOIN bacia ON estacao.BaciaCodigo = bacia.RegistroID ";
+    $sql = $pdo->query($sql);
+}catch(PDO_EXCEPTION $e){
+    echo "Falhou seleção: ".$e->getMessage();
+}
+
+$array_json = [];
+foreach ($sql as $key => $row) {
+    $array_json[$key]['roi_point'] = $row['Latitude'] ." ". $row['Longitude'];
+    $array_json[$key]['roi_name']  = utf8_encode($row['EstacaoNome']);
+    $array_json[$key]['roi_description']  = utf8_encode($row['BaciaNome']);
+};
+
+//print_r($array_json);                                            
+$json = json_encode($array_json);
+
+print_r($json);
 
 $cURL = curl_init(''); // url da api
 curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
@@ -24,17 +51,7 @@ echo "  " .$resultado;
 
 $resultado = true;
 if ($resultado) {   // resultado ok - retorno de sucesso da api
-    $dsn = "mysql:dbname=mpog_lmeo;host=127.0.0.1";
-    $dbuser = "root";
-    $dbpass = "";
-
-    try{
-        $pdo = new PDO($dsn,$dbuser,$dbpass);
-        //echo("Conexão estabelecida com sucesso");
-    } catch(PDO_EXCEPTION $e){
-        echo "Falhou: ".$e->getMessage();
-    }
-
+    
     $json = json_decode($data_string, true);
     //echo '<pre>' . print_r($json, true) . '</pre>';
     //print_r($json['data']);
